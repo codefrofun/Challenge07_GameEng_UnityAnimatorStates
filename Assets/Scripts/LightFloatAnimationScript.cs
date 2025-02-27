41a932ab955d5ddd2cc203f90d634f108843851a
Finlayson <W0506675@campus.nscc.ca>
2025-02-27 15:38:05 -0400

    Completed all 3 animations, finished code

--- Assets/Scripts/LightFloatAnimationScript.cs
+++ Assets/Scripts/LightFloatAnimationScript.cs
@@ -11,21 +11,59 @@ public class LightFloatAnimationScript : MonoBehaviour
     public float minPulseSpeed = 0f;
     public float maxPulseSpeed = 15f;
 
+    public Material yellowMaterial;
+    public Material greenMaterial;
+    public Material blueMaterial;
+
+    private Renderer cubeRenderer;
+
+    private Light cubeLight;
+
+    public float minLightIntensity = 1f;
+    public float maxLightIntensity = 10f;
+
     private void Start()
     {
         LightAnimationController = GetComponent<Animator>();
+        cubeRenderer = GetComponent<Renderer>();
+        cubeLight = GetComponentInChildren<Light>();
+
+        if (cubeLight != null)
+        {
+            cubeLight.intensity = minLightIntensity;
+        }
     }
 
     void Update()
     {
         float scrollInput = Input.mouseScrollDelta.y;
 
-        if(scrollInput != 0)
+        if (scrollInput != 0)
         {
-            pulseSpeed += scrollInput * 0.5f; // To adjust speed based on scroll input
+            // To adjust speed based on scroll input
+            pulseSpeed += scrollInput * 0.2f;
             pulseSpeed = Mathf.Clamp(pulseSpeed, minPulseSpeed, maxPulseSpeed);
 
             LightAnimationController.SetFloat("PulseSpeed", pulseSpeed);
+        }
+
+        if (pulseSpeed < 1)
+        {
+            cubeRenderer.material.color = yellowMaterial.color;
+        }
+        else if (pulseSpeed >= 1 && pulseSpeed < 2)
+        {
+            cubeRenderer.material.color = greenMaterial.color;
+        }
+        else
+        {
+            cubeRenderer.material.color = blueMaterial.color;
+        }
+
+        if (cubeLight != null)
+        {
+            //ping pong math to increase light intensity, depending on scroll speed
+            cubeLight.intensity = Mathf.PingPong(pulseSpeed, maxLightIntensity - minLightIntensity) + minLightIntensity;
         }
     }
 }
